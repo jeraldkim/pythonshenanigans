56dfd55fa8b090b9444ccb258b5fef75b4cac6cd
Jerald Kim <jerryckim@gmail.com>
2022-01-09 08:07:59 +0000

    만년하사의 노후 계산기

--- military_wealth_calculator.py
+++ military_wealth_calculator.py
@@ -1,2 +1,18 @@
-raise_rate = 0.03
-investment_ROI
+raise_rate = 0.03 #연봉 인상률
+investment_ROI = 0.08 #S&P500 평균 1년 수익
+yearly_salary = 20137200 #2022하사 연봉 20137.2
+salary_class_increase = 344400
+saving_rate = 0.3
+years_employed = 30
+money_saved = 0
+
+for i in range(1,years_employed):
+    print(money_saved)
+    money_saved *= (1 + investment_ROI)
+    salary_saved = yearly_salary * saving_rate
+    money_saved += salary_saved
+
+    yearly_salary += salary_class_increase
+    yearly_salary *= (1 + raise_rate)
+    salary_class_increase *= (1 + raise_rate)
+
